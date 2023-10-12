636f3f971076f582c2291c628904c1fd81e4401c
gulya <gulya@STEPAN>
2023-10-12 15:58:54 +0700

    Добавьте файлы проекта.

---
+++ laba3_2/laba3_2.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+#include <clocale>
+
+using namespace std;
+
+int main() {
+	setlocale(LC_ALL, "ru");
+	int first;
+	int second;
+	cout << "Введите первое число: ";
+	cin >> first;
+	cout << "Введите второе число: ";
+	cin >> second;
+	if (first > second) {
+		cout << "2";
+	}
+	else {
+		cout << "1";
+	}
+}
